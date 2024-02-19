f6767f2cf80c1c4eeb8ebf05e356546e33cdedbc
ankush-anonymous <canankushdoit@gmail.com>
2024-02-19 18:24:52 +0530

    pages&Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-4xl">Hello world!!!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
